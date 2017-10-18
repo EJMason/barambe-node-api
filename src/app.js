9b84d21d4b5aa08421fd42961ba51e4b534afdd0
EJMason <eliotjunior@gmail.com>
2017-10-17 23:20:32 -0700

    server now listens and shuts down gracefully

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 const config = require('config');
 const express = require('express');
+const http = require('http');
 const chalk = require('chalk');
 
+const utils = require('./utils/serverUtils');
+
 const app = express();
+const server = http.createServer(app);
 
 app.get('/test', (req, res) => {
-  res.status(200).send('ok');
+  res
+  .status(200)
+  .send('ok');
 });
 
-app.listen(config.port, () => {
-  console.log(`Listening on port ${chalk.magenta(config.port)}`);
-})
+// add root middleware
+
+// init event listeners
+utils.processCloseEvents(server, {});
+
+// routers
+
+// test db connection
+
+server.listen(config.port);
+
+// -------------- Listeners ---------------- //
+
+server.on('error', utils.onError);
+server.on('listening', () => utils.onListening(config.port, process.env.NODE_ENV));
+
+// ---- extra credit ---- //
+// add logger
---
+++ src/utils/serverUtils.js
@@ -0,0 +1,56 @@
+const chalk = require('chalk');
+// init event listeners
+module.exports.processCloseEvents = (serverInstance, dbConnection) => {
+  process.on('SIGINT', function onSigint () {
+    console.info(
+      'Got SIGINT (aka ctrl-c in docker). Graceful shutdown ',
+      new Date().toISOString()
+    );
+
+    serverInstance.close((err) => {
+      if (err) {
+        console.error('There was an error');
+        process.exitCode = 1;
+      }
+      process.exit();
+    });
+  });
+
+  process.on('SIGTERM', function onSigterm () {
+    console.info(
+      'Got SIGTERM (docker container stop). Graceful shutdown ',
+      new Date().toISOString()
+    );
+
+    serverInstance.close((err) => {
+      if (err) {
+        console.error('There was an error');
+        process.exitCode = 1;
+      }
+      process.exit();
+    });
+  });
+}
+
+module.exports.onError = (error) => {
+  if (error.syscall !== 'listen') throw error;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`The Server requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`Port is already in use or blocked by the os`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+module.exports.onListening = (port, nodeEnv) => {
+  console.log('');
+  console.log(`${chalk.blue('Server Listening on port ')}${chalk.yellow(port)}`);
+  console.log(`Environment: ${chalk.red(nodeEnv)}`);
+  console.log('-----------------------------------------------------------------');
+}
